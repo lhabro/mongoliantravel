f2a1b3c59a8406830d0cd8f551b94d84b88f9b29
lhabro <surenlhagva227@gmail.com>
2026-04-14 12:39:36 +0800

    Create index.js

---
+++ functions/api/auth/index.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+  const { env } = context;
+  
+  const params = new URLSearchParams({
+    client_id: env.GITHUB_CLIENT_ID,
+    scope: 'repo,user',
+    redirect_uri: 'https://mongoliantravel.xyz/api/auth/callback',
+  });
+
+  return Response.redirect(
+    `https://github.com/login/oauth/authorize?${params}`,
+    302
+  );
+}
